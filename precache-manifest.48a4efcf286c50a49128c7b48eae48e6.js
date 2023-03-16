7d01fbc1683c4ae0798f4df300a125aa8e635816
Code Sandbox <hello@codesandbox.io>
2023-03-16 17:31:46 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.48a4efcf286c50a49128c7b48eae48e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786a0be6df47e82e469f9e8025606965",
+    "url": "https://harshgupta84.github.io/csb-hbpbkd/index.html"
+  },
+  {
+    "revision": "eb4537179f6c682f6fa2",
+    "url": "https://harshgupta84.github.io/csb-hbpbkd/static/js/2.21b3566b.chunk.js"
+  },
+  {
+    "revision": "4194c99edb1c261fbcb8",
+    "url": "https://harshgupta84.github.io/csb-hbpbkd/static/js/main.13686512.chunk.js"
+  },
+  {
+    "revision": "1b6ca6913837596ff4d2",
+    "url": "https://harshgupta84.github.io/csb-hbpbkd/static/js/runtime-main.ec423b05.js"
+  }
+]);
